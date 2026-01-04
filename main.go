f1ec79d091d5085b3069af52b4fb087f53f55513
James Robins <knockknockitsjimmy@outlook.com>
2026-01-04 20:08:46 +1000

    Fixed CLI Flags

--- main.go
+++ main.go
@@ -14,14 +14,6 @@ import (
 
 func main() {
 
-	showVersion := flag.Bool("version", false, "print version information and exit")
-	flag.Parse()
-
-	if *showVersion {
-		fmt.Printf("norman %s\ncommit: %s\nbuilt:  %s\n", version.Version, version.Commit, version.Date)
-		return
-	}
-
 	adapters := []core.Adapter{
 		&postgres.PostgresAdapter{},
 		&mysql.MySqlAdapter{},
@@ -31,11 +23,57 @@ func main() {
 		&reports.MermaidReportWriter{},
 	}
 
+	var showVersion = flag.Bool("version", false, "print version information and exit")
+	var outputDir = flag.String("output-dir", "./norman/", "Directory to output reports to")
+	var connStr = flag.String("conn", "", "Database connection string " + driverOptionHelperString(adapters) + " (required)")
+	var reportCsv = flag.String("report-types", "all", "Comma-separated list of report types to generate " + reportOptionHelperString(reports))
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Printf("norman %s\ncommit: %s\nbuilt:  %s\n", version.Version, version.Commit, version.Date)
+		return
+	}
+
 	runner := core.NewRunner(adapters, reports)
-	err := runner.Run()
+	err := runner.Run(connStr, outputDir, reportCsv)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
 	os.Exit(0)
+}
+
+func driverOptionHelperString(adapters []core.Adapter) string {
+	var driverOptionsString = "(Drivers Available: "
+	for i, a := range adapters {
+		if i > 0 {
+			driverOptionsString += ", "
+		}
+		driverOptionsString += a.UniqueSignature()
+	}
+	driverOptionsString += ")"
+	return driverOptionsString
+}
+
+
+func reportOptionHelperString(reports []core.InventoryReportWriter) string {
+	var reportOptionsString = "("
+	for i, r := range reports {
+		for _, key := range r.GetReportKeys() {
+
+			if i > 0 {
+				reportOptionsString += ", "
+			}
+			reportOptionsString += key
+		}
+
+		if i == len(reports) - 1 {
+			reportOptionsString += ","
+		}
+	}
+
+
+
+	reportOptionsString += " all)"
+	return reportOptionsString
 }
